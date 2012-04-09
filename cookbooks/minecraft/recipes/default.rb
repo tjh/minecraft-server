b969e2af8bd27d7b6ecfd346fbf94b98432132d3
Tim Harvey <tim@literacy5.com>
2012-04-08 20:34:05 -0400

    Switch to init.d script and tweak server settings
    * Max of 8 players
    
    * No more PVP
    
    * Whitelist players only
    
    * Setup ops and white lists

--- cookbooks/minecraft/recipes/default.rb
+++ cookbooks/minecraft/recipes/default.rb
@@ -1,17 +1,13 @@
 minecraft_directory = '/mnt/data_minecraft'
+backup_directory = '/mnt/backup_minecraft'
 
-directory minecraft_directory do
-  owner 'ubuntu'
-  group 'ubuntu'
-  mode '0755'
+[minecraft_directory, backup_directory].each do |folder|
+  directory folder do
+    owner 'ubuntu'
+    group 'ubuntu'
+    mode '0755'
+  end
 end
-
-#mount '/mnt/data_minecraft' do
-  #action [:mount, :enable]  # mount and add to fstab
-  #device 'data_minecraft'
-  #device_type :label
-  #options 'noatime,errors=remount-ro'
-#end
 
 remote_file "#{minecraft_directory}/minecraft_server.jar" do
   source "http://www.minecraft.net/download/minecraft_server.jar"
@@ -25,14 +21,16 @@ cookbook_file "#{minecraft_directory}/minecraft.sh" do
   mode "0744"
 end
 
-cookbook_file "#{minecraft_directory}/server.properties" do
-  source 'server.properties'
-  owner  'ubuntu'
-  group  'ubuntu'
+%w{ops.txt white-list.txt server.properties}.each do |filename|
+  cookbook_file "#{minecraft_directory}/#{filename}" do
+    source filename
+    owner  'ubuntu'
+    group  'ubuntu'
+  end
 end
 
-cookbook_file "/etc/rc.local" do
-  source 'rc.local'
+cookbook_file "/etc/init.d/minecraft" do
+  source 'minecraft'
   owner  'ubuntu'
   group  'ubuntu'
   mode "0755"
